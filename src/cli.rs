a711c8a5fd3af29b16a6990badb69af2117728cd
Tai Danh <tai@danh.gg>
2023-12-06 14:40:51 -0800

    Clarifies CLI commands

--- src/cli.rs
+++ src/cli.rs
@@ -2,17 +2,33 @@ use clap::Subcommand;
 
 #[derive(Subcommand)]
 pub enum CellarCommand {
+    /// Adds a new executable
+    ///
+    /// Arguments:
+    ///     <exe> - The path to the executable to add
     Add {
+        #[arg(short, long, help = "Path to the executable to add")]
         exe: String,
     },
 
+    /// Runs a specified command
+    ///
+    /// Arguments:
+    ///     <command_name> - Name of the command to run
     Run {
+        #[arg(short, long, help = "Name of the command to run")]
         command_name: String,
     },
 
+    /// Removes an existing command
+    ///
+    /// Arguments:
+    ///     <command_name> - Name of the command to remove
     Remove {
+        #[arg(short, long, help = "Name of the command to remove")]
         command_name: String,
     },
 
+    /// Initializes the configuration directory
     Init {},
 }
